dff3be87fed0d7e0d075e3118daee513e8997e55
Timo Aeckerle <timoaeckerle@gmx.de>
2020-03-25 21:44:37 +0100

    still testing

--- server.config.mjs
+++ server.config.mjs
@@ -5,7 +5,7 @@ import {fileURLToPath} from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
-  port: 8080,
+  port: 8081,
   dir: join(__dirname, 'dist'),
   index: join(__dirname, 'dist', 'index.html')
 }
